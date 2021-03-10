88523d0e9bb0568497c706dc34d50af2eb913d4c
Jesse Wenger <wengerj@gmail.com>
2021-03-10 21:03:31 +0000

    Automatically backed up by Learn

--- lib/concerns/paramable.rb
+++ lib/concerns/paramable.rb
@@ -1,3 +1,12 @@
-  def to_param
-    name.downcase.gsub(' ', '-')
+module Paramable
+  module ClassMethods
+    def to_param
+      name.downcase.gsub(' ', '-')
+    end
+    def to_param
+      name.downcase.gsub(' ', '-')
+    end
   end
+  module InstanceMethods 
+  end
+end
