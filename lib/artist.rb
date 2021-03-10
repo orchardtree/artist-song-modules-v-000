9b3321a4fef0414fe4b43051d51c4566c58670e9
Jesse Wenger <wengerj@gmail.com>
2021-03-10 21:20:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,23 +15,11 @@ class Artist
     super
     @songs = []
   end
-=begin
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-=end
+
   def self.all
     @@all
   end
-=begin
-  def self.reset_all
-    self.all.clear
-  end
 
-  def self.count
-    self.all.count
-  end
-=end
   def add_song(song)
     @songs << song
     song.artist = self
@@ -40,9 +28,4 @@ class Artist
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-=begin
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-=end
 end
