48e27f25de59feed423bb755908607577b182479
Jesse Wenger <wengerj@gmail.com>
2021-03-10 20:44:14 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,8 +1,14 @@
 module Memorable
-  module InstanceMethods
-    
-  end
   module ClassMethods
-    
+    def self.reset_all
+      self.all.clear
+    end
+
+    def self.count
+      self.all.count
+    end
+  end
+  
+  module InstanceMethods
   end
 end
