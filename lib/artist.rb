ebe65bd4337b0bdcd707a758ffd16f6bdac97a7c
Jesse Wenger <wengerj@gmail.com>
2021-03-10 21:11:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Artist
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
-  extend Paramable::ClassMethods
+  include Paramable::InstanceMethods
   
   attr_accessor :name
   attr_reader :songs
